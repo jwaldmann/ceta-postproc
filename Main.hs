dcc3aea43bd361d09e61f4e5d182940874580a7d
Johannes Waldmann <waldmann@imn.htwk-leipzig.de>
2014-05-17 18:39:31 +0200

    with conformity checks

--- Main.hs
+++ Main.hs
@@ -12,6 +12,8 @@ the first file contains stdin and stderr merged,
 and prepended with timestamps.
 
 these modifications (C) Johannes Waldmann 2014
+
+original copyright notice follows:
 -}
 
 {-
@@ -32,41 +34,73 @@ You should have received a copy of the GNU Lesser General Public License along
 with IsaFoR/CeTA. If not, see <http://www.gnu.org/licenses/>.
 -}
 
+{-# language OverloadedStrings #-}
+
 module Main(main) where 
 
 import qualified Ceta -- the certifier
 
 import TPDB.Input (get_trs)
+import TPDB.Pretty
+import TPDB.CPF.Proof.Type as CPF
+import TPDB.CPF.Proof.Read as CPF
 
 import System.Environment -- for getArgs
 import System.IO -- for file reading
 import System.Exit -- for error codes
+import Control.Monad ( when )
 
 main = do 
     args <- getArgs
     case args of
-        [ outfile, benchfile ] -> do
+        [ outfile, benchfile ] -> handle True outfile benchfile
+        [ "-n", outfile, benchfile ] -> handle False outfile benchfile
 
-            bench <- TPDB.Input.get_trs benchfile
-            
-            stamped_out <- readFile outfile
-            let claim : out = map remove_timestamp $ lines $ stamped_out
-                problemString = unlines out
+handle remove_stamps outfile benchfile = do
+    bench <- TPDB.Input.get_trs benchfile
+    
+    out <- readFile outfile
+    let process = if remove_stamps then remove_timestamp else id
+        claim_string : proof = map process $ lines out
+        claim = case claim_string of
+              "YES" -> Just True
+              "NO"  -> Just False
+              _     -> Nothing
+        problemString = unlines proof
 
-                (status, msg) = certify False $ problemString
-            
-            print status
-            putStrLn msg
+        (cert, msg) = certify False $ problemString
+
+    [ p ] <- CPF.readCP problemString
+
+    -- claim conforms to proof
+    when ( bench /= CPF.trsinput_trs ( CPF.input p ) )
+         $ whine INPUT_MISMATCH 
+         $ vcat [ "benchmark:" <+> pretty bench
+                , "proof.input:" <+> pretty ( CPF.trsinput_trs $ CPF.input p ) ]
+
+    -- input from benchfile conforms to input from proof
+    case (claim, CPF.proof p) of
+         (Just True , CPF.TrsTerminationProof {} ) -> return ()
+         (Just False, CPF.TrsNonterminationProof {} ) -> return ()
+         _ -> whine CLAIM_MISMATCH $ pretty claim
+    
+    whine cert $ text msg     
 
 remove_timestamp :: String -> String
 remove_timestamp = unwords . drop 1 . words
+
+whine :: Status -> Doc -> IO ()
+whine status doc = do
+    putStrLn $ show status
+    case status of
+        CERTIFIED -> print doc
+        _ -> error $ show doc
 
 data Status = INPUT_MISMATCH
             | CLAIM_MISMATCH
             | CERTIFIED | REJECTED | UNSUPPORTED | UNKNOWN_ERROR
   deriving (Eq, Show )
             
-
 certify a problemString = 
     case Ceta.certify_proof a problemString of
          Ceta.Sumbot (Ceta.Inr (Ceta.Certified prf)) -> 
