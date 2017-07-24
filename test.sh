1918e8a7f753b550ba8440994fdc11b5dae1e494
Johannes Waldmann <johannes.waldmann@htwk-leipzig.de>
2017-07-24 15:25:29 +0200

    rewrite/add test cases for #11

--- test.sh
+++ test.sh
@@ -2,10 +2,13 @@
 
 stack install
 
-ceta-postproc -n data/z25ok.cpf data/z25.xml
-ceta-postproc -n data/z27.cpf data/z27.xml
-ceta-postproc -n data/z042.cpf data/z042.xml
+cepp () {
+    cat $1 | tail -n+2 | ceta-postproc $2 /dev/stdin
+}
 
-ceta-postproc -n data/AC28.cpf data/AC28.xml
-
-ceta-postproc -n data/3.39.cpf data/3.39.xml
+cepp data/z25ok.cpf data/z25.xml
+cepp data/z27.cpf data/z27.xml
+cepp data/z042.cpf data/z042.xml
+cepp data/AC28.cpf data/AC28.xml
+cepp data/3.39.cpf data/3.39.xml
+cepp data/AG01-3.24.cpf data/AG01-3.24.xml
