b4021fa7d8d12213d12b8531b59e2c2b38a896df
Johannes Waldmann <johannes.waldmann@htwk-leipzig.de>
2017-07-24 15:31:56 +0200

    update test cases

--- test.sh
+++ test.sh
@@ -3,8 +3,11 @@
 stack install
 
 cepp () {
+    echo ================= $1 $2
     cat $1 | tail -n+2 | ceta-postproc $2 /dev/stdin
 }
+
+echo "must work:"
 
 cepp data/z25ok.cpf data/z25.xml
 cepp data/z27.cpf data/z27.xml
@@ -12,3 +15,7 @@ cepp data/z042.cpf data/z042.xml
 cepp data/AC28.cpf data/AC28.xml
 cepp data/3.39.cpf data/3.39.xml
 cepp data/AG01-3.24.cpf data/AG01-3.24.xml
+
+echo "must fail:"
+
+cepp data/z25wrong.cpf data/z25.xml
