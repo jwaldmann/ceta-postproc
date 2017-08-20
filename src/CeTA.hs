17695d73d8cb6b2586d8f250f1ef943f15d87082
Johannes Waldmann <johannes.waldmann@htwk-leipzig.de>
2017-08-20 18:49:18 +0200

    use official 2.31 source

--- src/CeTA.hs
+++ src/CeTA.hs
@@ -1,6 +1,7 @@
 {-
 Author:  Christian Sternagel <c.sternagel@gmail.com> (2009-2015, 2017)
 Author:  René Thiemann <rene.thiemann@uibk.ac.at> (2009-2014)
+Author:  Akihisa Yamada <akihisa.yamada@uibk.ac.at> (2017)
 License: LGPL (see file COPYING.LESSER)
 -}
 module Main (main) where
@@ -12,42 +13,40 @@ import System.Exit -- for error codes
 
 main = getArgs >>= parse False
 
-parse _ ("--version":_) = do putStrLn version
+parse _ ("--version":_) = do putStrLn Main.version
 parse _ ("--allow-assumptions":args) = parse True args
-parse flag [xtc,cpf] = do
+parse flag [xtc,claim,cpf] = do
   problemString <- readFile xtc
+  claimString <- readFile claim
   proofString <- readFile cpf
-  start flag (Just problemString) proofString
+  start flag (Just problemString) (Inr claimString) proofString
 
 parse flag [cpf] = do
   problemString <- readFile cpf
-  start flag Nothing problemString
+  start flag Nothing (Inl Anything) problemString
 
 parse _ _ = do
   hPutStrLn stderr usage
   exitWith (ExitFailure 4)
 
-start a problemString proofString = 
-    do case certify_proof a problemString proofString of
-         Sumbot (Inr (Certified prf)) -> 
-             do putStrLn ("CERTIFIED " ++ prf)
+start a problemString claimString proofString = 
+    do case certify_proof a problemString claimString proofString of
+         Certified -> 
+             do putStrLn ("CERTIFIED")
                 exitWith ExitSuccess
-         Sumbot (Inr (Error message)) -> 
+         Error message -> 
              do putStrLn "REJECTED"
                 hPutStrLn stderr message
                 exitWith (ExitFailure 1)
-         Sumbot (Inr (Unsupported message)) -> 
+         Unsupported message -> 
              do putStrLn "UNSUPPORTED" 
                 hPutStrLn stderr message
                 exitWith (ExitFailure 2)
-         Sumbot (Inl message) -> 
-             do putStrLn "UNKNOWN ERROR"
-                hPutStrLn stderr message
-                exitWith (ExitFailure 3)
 
 usage = "usage: ceta [[--allow-assumptions] certificate | --version]\n\
         \  \n\
         \  A \"certificate\" is an XML file in certification problem format (CPF).\n\
         \  \n\
         \  --allow-assumptions    Allow (axiomatic) assumptions in the certificate.\n\
-        \  --version              Print the version number (+ mercurial id)."
+        \  --version              Print the version number."
+version = "2.31"
